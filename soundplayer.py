be846126dc13fa02c2c6950daa29af122e3f9188
marcouxjohn <40047408+marcouxjohn@users.noreply.github.com>
2019-11-19 15:18:20 -0400

    Fixed issue from Issue list

--- soundplayer.py
+++ soundplayer.py
@@ -10,59 +10,37 @@ import _thread
 import pyaudio
 
 # Might need to add a method for converting mp3's to wavs
+#   Probably gonna use ffmpeg via pydub
 # Or figure out how to fire an mp3 through pyaudio
+#   Not sure if I can
 
 class SoundPlayer:
-    def __init__(self, queue_size, chunk_size):
+    def __init__(self, chunk_size):
         self.py_audio = pyaudio.PyAudio()
         self.chunk_size = chunk_size
-        self.queue_size = queue_size
-        self.num_stored = 0
-        self.stored = {}
 
     def get_sound(self, sound_name):
         try:
-            data = self.stored[sound_name]
-            del self.stored[sound_name]
-            self.stored[sound_name] = data
+            data = wave.open(sound_name, 'rb')
+        except:
+            return None
+        try:
             stream = self.py_audio.open(
-                format=self.py_audio.get_format_from_width(data.getsampwidth()),
+                format=self.py_audio.get_format_from_width(
+                    data.getsampwidth()),
                 channels=data.getnchannels(),
                 rate=data.getframerate(),
                 output=True)
         except:
-            try:
-                data = wave.open(sound_name, 'rb')
-            except:
-                stream.close()
-                return None
-            try:
-                stream = self.py_audio.open(
-                    format=self.py_audio.get_format_from_width(
-                        data.getsampwidth()),
-                    channels=data.getnchannels(),
-                    rate=data.getframerate(),
-                    output=True)
-            except:
-                return None
-            if self.num_stored == self.queue_size:
-                rem = list(self.stored)[0]
-                del self.stored[rem]
-                self.stored[sound_name] = data
-            else:
-                self.stored[sound_name] = data
-                self.num_stored += 1
+            data.close()
+            return None
         return data, stream
 
     def play_sound(self, sound_name):
-        # Might have to slap this in a thread
-        # Might need a global 'play' function to play and overlay anything
-        # That gets sent to it, but threading might solve this issue
         sound, stream = self.get_sound(sound_name)
         _thread.start_new_thread(thread_sound, (sound, stream, self.chunk_size))
 
     def destroy(self):
-        del self.stored
         self.py_audio.terminate()
 
 def thread_sound(sound, stream, chunk_size):
@@ -75,17 +53,75 @@ def thread_sound(sound, stream, chunk_size):
         data = sound.readframes(chunk_size)
     stream.stop_stream()
     stream.close()
+    print("DONE")
     return True
 
-my_player = SoundPlayer(10, 1024)
-#my_player.play_sound("SampleAudio_0.4mb.mp3")
+my_player = SoundPlayer(1024)
 my_player.play_sound("file_example_WAV_1MG.wav")
 time.sleep(1)
 my_player.play_sound("file_example_WAV_1MG.wav")
-time.sleep(30)
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+time.sleep(1)
+my_player.play_sound("file_example_WAV_1MG.wav")
+for i in range(35):
+    print(i)
+    time.sleep(1)
 my_player.destroy()
 del my_player
-#stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-#                channels=wf.getnchannels(),
-#                rate=wf.getframerate(),
-#                output=True)
