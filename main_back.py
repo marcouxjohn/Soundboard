c889d99738c5c8138eb197c7baeaaf0a6e9d3d3d
marcouxjohn <40047408+marcouxjohn@users.noreply.github.com>
2019-11-19 22:06:16 -0400

    Comments and stuff

--- main_back.py
+++ main_back.py
@@ -4,6 +4,8 @@
 # VERSION:  1.0
 # PURPOSE:  Main source for front end to interact via
 
+# Import all the libs we need
+
 import time
 import glob
 import os
@@ -12,7 +14,25 @@ from shutil import copyfile
 import soundplayer
 import fileFunctions
 
+"""
+Name:       BackEnd
+Purpose:    Back End object for all playing and tracking of sounds
+Notes:      None
+"""
+
 class BackEnd:
+
+    """
+    Name:       __init__
+    Purpose:    Initializes a BackEnd object
+    Arguments:  The chunk size for playing an audio file optionally
+    Output:     None
+    Modifies:   Creates the aforementioned object
+    Returns:    The object
+    Assumptions:None
+    Bugs:       None
+    """
+
     def __init__(self, chunk_size=1024):
         self.sound_player = soundplayer.SoundPlayer(chunk_size)
         self.doc_root = os.getcwd()
@@ -25,13 +45,40 @@ class BackEnd:
                                      self.dirsep + "*.[mp3|wav]")
         for i in range(len(self.sound_names)):
             self.sound_names[i] = self.sound_names[i].split(self.dirsep)[-1]
+    
+    """
+    Name:       play_sound
+    Purpose:    Plays a sound via the SoundPlayer object
+    Arguments:  The name of the sound
+    Output:     None
+    Modifies:   Writes to a sound output stream
+    Returns:    True on success, False on fail
+    Assumptions:None
+    Bugs:       None
+    """
+    
     def play_sound(self, sound_name):
         if not sound_name in self.sound_names:
             return False
         return self.sound_player.play_sound(sound_name)
+
+    """
+    Name:       add_sound
+    Purpose:    Adds a sound to the list of current sounds
+    Arguments:  The path to the new sound
+    Output:     None
+    Modifies:   Adds the sound to the list of sounds
+    Returns:    True on success, False on failure
+    Assumptions:None
+    Bugs:       None
+    """
+
     def add_sound(self, path_to_new_sound):
         try:
             filename = path_to_new_sound.split(self.dirsep)[-1]
+            
+            # Make sure file isn't a dup, if it is make sure it's obvious
+            
             if filename in self.sound_names:
                 filename = filename.split('.')
                 filename[0] += "(dup)"
@@ -44,6 +91,18 @@ class BackEnd:
             return True
         except:
             return False
+
+    """
+    Name:       rem_sound
+    Purpose:    Removes a sound from the sound list
+    Arguments:  The name of the sound
+    Output:     None
+    Modifies:   Removes the sound from the list if it is there
+    Returns:    True on success, False on failure
+    Assumptions:None
+    Bugs:       None
+    """
+
     def rem_sound(self, sound_name):
         if not sound_name in self.sound_names:
             return False
--- soundplayer.py
+++ soundplayer.py
@@ -1,8 +1,10 @@
 # FILE:     soundPlayer.py
 # AUTHOR:   John Marcoux
 # DATE:     2019/11/19
-# VERSION:  1.0
+# VERSION:  1.1
 # PURPOSE:  File for playing and overlaying sounds
+
+# Imports of all the libs we need
 
 import wave
 import time
@@ -11,19 +13,52 @@ import pyaudio
 
 # Might need to add a method for converting mp3's to wavs
 #   Probably gonna use ffmpeg via pydub
-# Or figure out how to fire an mp3 through pyaudio
-#   Not sure if I can
+
+"""
+Name:       SoundPlayer
+Purpose:    Allows playing sounds from disk
+Notes:      Has issues if more than ~30 sounds are playing at once
+"""
 
 class SoundPlayer:
+
+    """
+    Name:       __init__
+    Purpose:    Initializes a SoundPlayer object
+    Arguments:  The chunk size for playing an audio file
+    Output:     None
+    Modifies:   Creates the aforementioned object
+    Returns:    The object
+    Assumptions:None
+    Bugs:       None
+    """
+
     def __init__(self, chunk_size):
         self.py_audio = pyaudio.PyAudio()
         self.chunk_size = chunk_size
 
+    """
+    Name:       get_sound
+    Purpose:    Creates the sound output stream and wave file stream
+    Arguments:  The name of the sound
+    Output:     None
+    Modifies:   Creates the aforementioned objects
+    Returns:    The objects, or (None, None) on fail
+    Assumptions:None
+    Bugs:       None
+    """
+
     def get_sound(self, sound_name):
+        
+        # Get the audio
+        
         try:
             data = wave.open(sound_name, 'rb')
         except:
             return (None,None)
+        
+        # Set up the output stream
+        
         try:
             stream = self.py_audio.open(
                 format=self.py_audio.get_format_from_width(
@@ -36,27 +71,69 @@ class SoundPlayer:
             return (None,None)
         return data, stream
 
+    """
+    Name:       play_sound
+    Purpose:    Plays the given sound file if it is present on disk
+    Arguments:  The name of the sound
+    Output:     None
+    Modifies:   Creates a new thread to play the sound
+    Returns:    False on fail True on success
+    Assumptions:None
+    Bugs:       None
+    """
+
     def play_sound(self, sound_name):
         sound, stream = self.get_sound(sound_name)
+        
+        # Make sure the sound was grabbed properly
+        
         if sound is None and stream is None:
             return False
         _thread.start_new_thread(thread_sound, (sound, stream, self.chunk_size))
         return True
 
+    """
+    Name:       destroy
+    Purpose:    Destroys the current object
+    Arguments:  None
+    Output:     None
+    Modifies:   Destroys the object
+    Returns:    None
+    Assumptions:None
+    Bugs:       None
+    """
+
     def destroy(self):
         self.py_audio.terminate()
 
+
+
+"""
+Name:       thread_sound
+Purpose:    Function to pass to a thread to play the sound
+Arguments:  The sound file stream, the output stream, and the chunk size
+Output:     None
+Modifies:   Writes to the output stream
+Returns:    False on fail, True on success
+Assumptions:None
+Bugs:       None
+"""
+
 def thread_sound(sound, stream, chunk_size):
+    
+    # Make sure sound is there
+    
     if sound is None:
         return False
-    sound.rewind()
+
+    # Play the sound reading "chunk_size" bytes at a time
+
     data = sound.readframes(chunk_size)
     while len(data) > 0:
         stream.write(data)
         data = sound.readframes(chunk_size)
     stream.stop_stream()
     stream.close()
-    print("DONE")
     return True
 
 if __name__ == "__main__":
