14dcf5bd9ae3d6c4b5b95c802af800734509ba7b
Maggie <49000013+MaggiePerry@users.noreply.github.com>
2019-11-27 14:37:09 -0400

    Added comments and fixed some more issues

--- Elements.py
+++ Elements.py
@@ -1,8 +1,31 @@
+# FILE:     Elements.py
+# AUTHOR:   Margaret Perry
+# DATE:     2019/11/17
+# VERSION:  1.0
+# PURPOSE:  Creates Background as sprites for pygame
+
 import pygame
 
+"""
+Name:       Background
+Purpose:    Generates a background sprite
+Notes:      None
+"""
+
 class Background(pygame.sprite.Sprite):
+    """
+    Name:        __init__
+    Purpose:     Initalizes a background sprite
+    Arguments:   image file, x y coodinates
+    Output:      None
+    Modifies:    None
+    Returns:     Background sprite
+    Assumptions: None
+    Bugs:        None
+    """
+
     def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        pygame.sprite.Sprite.__init__(self)  # Call Pygame Sprite initializer
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
