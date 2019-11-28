90606192accd4ffb2117cae9e16d2bc114f77f85
Maggie <49000013+MaggiePerry@users.noreply.github.com>
2019-11-28 15:02:46 -0400

    Final Comments added

--- Elements.py
+++ Elements.py
@@ -25,7 +25,7 @@ class Background(pygame.sprite.Sprite):
     """
 
     def __init__(self, image_file, location):
-        pygame.sprite.Sprite.__init__(self)  # Call Pygame Sprite initializer
-        self.image = pygame.image.load(image_file)
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
+        pygame.sprite.Sprite.__init__(self)        # Call Pygame Sprite initializer
+        self.image = pygame.image.load(image_file) # Loads given image
+        self.rect = self.image.get_rect()          # Draws a rectangle to fit the background
+        self.rect.left, self.rect.top = location   # Positions image at given coodinates
